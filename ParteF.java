cf05cd9916a4259db46c60c1cff2a3c66cbbb612
Richard Eduardo Riveros Navas <rriveros@unicartagena.edu.co>
2026-09-15 18:47:46 -0500

    ParteF

---
+++ ParteF.java
@@ -0,0 +1,18 @@
+public class ParteF {
+    public class MainParteF {
+        public static void main(String[] args) {
+            // Ejercicio 30
+            Estudiante e1 = new Estudiante();
+            Estudiante e2 = new Estudiante("Laura", 1204);
+            Estudiante e3 = new Estudiante("Carlos", 1305, 5);
+
+            // Ejercicio 32
+            Producto p1 = Producto.crearProductoBasico("Cargador");
+            Producto p2 = Producto.crearProductoBasico("Audífonos");
+
+            // Ejercicio 34
+            Libro original = new Libro("Rayuela", "Julio Cortázar", 600);
+            Libro copia = new Libro(original);
+        }
+    }
+}
