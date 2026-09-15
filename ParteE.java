7fca4b3f9ce81fad55883ba0357eb7bd114477bc
Richard Eduardo Riveros Navas <rriveros@unicartagena.edu.co>
2026-09-15 18:46:40 -0500

    ParteE

---
+++ ParteE.java
@@ -0,0 +1,18 @@
+public class ParteE {
+    public static void main(String[] args) {
+            // Ejercicio 25
+            Libro vacio = new Libro();
+            Libro conDatos = new Libro("Rayuela", "Julio Cortázar", 600);
+
+            // Ejercicio 26
+            Producto pVacio = new Producto();
+            Producto pConDatos = new Producto("Mouse", 25000, 30);
+
+            // Ejercicio 28
+            CuentaBancaria c3 = new CuentaBancaria("111", "Pedro", 200000);
+            CuentaBancaria c4 = new CuentaBancaria("222", "Ana", 500000);
+            CuentaBancaria c5 = new CuentaBancaria("333", "Luis", 100000);
+        }
+    }
+
+
